af1cbd1716a1a5e71c546d9b99bdc9c30f2ac804
LeoDxy <Leo_duanxy@163.com>
2018-03-01 13:43:53 +0800

    TEST

--- income_regression.py
+++ income_regression.py
@@ -7,14 +7,16 @@ from sklearn.tree import DecisionTreeRegressor
 
 data = pd.read_csv('income_regression.csv')
 target = data.income
-features_data = data.drop('income',axis=1)
+features_data = data.drop('income', axis=1)
 
- # 提取数值类型为整数或浮点数的变量
+#  提取数值类型为整数或浮点数的变量
 numeric_features = [c for c in features_data if features_data[c].dtype.kind in ('i', 'f')]
 numeric_data = data[numeric_features]
 
-# 类别数据处理
+# 类别特征数据处理
 le = preprocessing.LabelEncoder()
+
+
 def data_encoded(x):
     le.fit(x)
     return le.transform(x)
@@ -23,7 +25,8 @@ categorical_data = features_data.drop(numeric_features,axis=1)
 categorical_data_encoded = categorical_data.apply(data_encoded)
 gender = categorical_data_encoded.gender
 categorical_data_encoded = categorical_data_encoded.drop('gender',axis=1)
-#  取对数  特征gender没有取对数
+
+#  取对数 特征gender没有取对数
 def d_log(x):
     if x:
         return np.log2(x)
@@ -32,9 +35,8 @@ def d_log(x):
 categorical_data_encoded = categorical_data_encoded.applymap(d_log)
 
 features = pd.concat([numeric_data, categorical_data_encoded, gender], axis=1)
-features.head()
 # 归一化
-scaler  = preprocessing.StandardScaler().fit(features)
+scaler = preprocessing.StandardScaler().fit(features)
 featrues_trans = scaler.transform(features)
 
 # 切分训练集测试集
@@ -46,4 +48,4 @@ score = cls.score(x_test, y_test)
 print("Trainting score:%f" % (cls.score(x_train, y_train)))
 print("预测值:%s" %cls.predict(x_test))
 y_hat = cls.predict(x_test)
-print(np.mean(np.abs(y_hat - y_test)))
+print(np.mean((y_hat - y_test)**2))
